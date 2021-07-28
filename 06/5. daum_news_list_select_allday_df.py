df9074d99c37efda83ad40b7bde51eacb354e159
AnSangYeong <asy19800510@gmail.com>
2021-07-28 21:09:48 +0900

    update code

--- 06/5. daum_news_list_select_allday_df.py
+++ 06/5. daum_news_list_select_allday_df.py
@@ -2,9 +2,6 @@ from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 from itertools import count
-from collections import OrderedDict
-import openpyxl
-import re
 import os
 
 def scrape_it_news(date_input):
